08d6f79d974b6333116f885afe782eaddd416474
Anirudh Singh <anirudhrathore2003@gmail.com>
2025-08-11 22:25:25 +0530

    fix: disable lint and typescript errors during build for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true, // Ignore TS errors on build
-  },
   eslint: {
-    ignoreDuringBuilds: true, // Ignore ESLint errors on build
+    ignoreDuringBuilds: true, // Ignore ESLint errors during build
+  },
+  typescript: {
+    ignoreBuildErrors: true, // Ignore TypeScript errors during build
   },
 };
 
